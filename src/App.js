8d0d86f3e1003f13c4fdccfe6328ad754e490b78
Marina Golitsyn <aelmorka@gmail.com>
2023-08-03 00:11:34 +0300

    First part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+import enAlphabet from './components/alphabet.json'
+
+import Letters from './components/Letters'
+import Solution from './components/Solution'
+import Scores from './components/Scores'
 
 function App() {
+  let cont = {word: "easyaa", hint: "Some text"}
+  cont.filled = Array(cont.word.length).fill("_")
+  let [alphabet, setAlphabet] = useState(enAlphabet)
+  let [game, setGame] = useState(cont)
+  
+  function checkLetterInWord(letter) {
+    if (game.word.toUpperCase().includes(letter)) {
+      let newGame = {...game}
+      newGame.filled = game.word.split("").map((el, index) => el.toUpperCase() === letter ? letter : game.filled[index])
+      setGame(newGame)
+    } 
+  }
+  function removeLetter(letter) {
+    let newAlphabet = {...alphabet}
+    let index = newAlphabet.letters.findIndex(el => el.name === letter)
+    newAlphabet.letters[index].checked = true
+    setAlphabet(newAlphabet)
+  }
+
+  function checkLetter(letter) {
+    checkLetterInWord(letter) 
+    removeLetter(letter)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hangman">
+      <Scores scores="100" />
+      <Solution game={game}/>
+      <Letters alphabet={alphabet.letters} checked={checkLetter}/>
     </div>
   );
 }
